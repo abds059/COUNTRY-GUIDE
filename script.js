9e451d202808e99f3cdea1e614c8d54cb64f1acf
abds059 <abdurrehmans059@gmail.com>
2025-03-09 16:37:48 +0500

    Implemented js functionality and api call

---
+++ script.js
@@ -0,0 +1,86 @@
+let btn = document.getElementById("search_btn");
+let apicontent = document.querySelector(".apicontent");
+
+const copy_url = `https://restcountries.com/v3.1/name/Pakistan?fullText=true`;
+
+console.log(copy_url);
+
+btn.addEventListener("click", async () => {
+  let country = document.getElementById("inp_box").value;
+
+  const url = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
+  console.log(url);
+
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let countrydata = data[0];
+    // console.log(countrydata);
+
+    let capital = countrydata.capital?.[0] || "N/A";
+    let borders = countrydata.borders?.join(" ,") || "No borders";
+    let flag =
+      countrydata.flags?.png || countrydata.flags?.svg || "No flag available";
+    let population = countrydata.population;
+    let languages = countrydata.languages
+      ? Object.values(countrydata.languages).join(" ,")
+      : "N/A";
+    let googleMap = countrydata.maps?.googleMaps || "No Google Map Available";
+    let openStreetMap =
+      countrydata.maps?.openStreetMaps || "No OpenStreetMap Available";
+    let area = countrydata.area ? `${countrydata.area}` : "N/A";
+    let region = countrydata.region || "N/A";
+    let timezones = countrydata.timezones?.join(", ") || "N/A";
+
+    apicontent.innerHTML = "";
+
+    let countryflag = document.createElement("img");
+    countryflag.src = flag;
+    countryflag.style.width = "60px";
+    countryflag.style.height = "50px";
+    countryflag.style.border = "2px solid gray";
+
+    let capital_el = document.createElement("p");
+    capital_el.innerText = `Capital: ${capital}`;
+
+    let borders_el = document.createElement("p");
+    borders_el.innerText = `Borders: ${borders}`;
+
+    let population_el = document.createElement("p");
+    population_el.innerText = `Population: ${population}`;
+
+    let languages_el = document.createElement("p");
+    languages.innerText = `Languages: ${languages}`;
+
+    let area_el = document.createElement("p");
+    area_el.innerText = `Area: ${area}`;
+
+    let region_el = document.createElement("p");
+    region_el.innerText = `Region: ${region}`;
+
+    let timezones_el = document.createElement("p");
+    timezones_el.innerText = `Time Zones: ${timezones}`;
+
+    let map_el = document.createElement("p");
+    map_el.innerHTML = ` <a href="${googleMap}" target="_blank">Google Maps</a> |
+                            <a href="${openStreetMap}" target="_blank">OpenStreetMap</a>`;
+    map_el.querySelectorAll("a").forEach((link) => {
+      link.style.textDecoration = "none";
+      link.style.color = "#3366cc";
+    });
+
+    apicontent.appendChild(countryflag);
+    apicontent.appendChild(capital_el);
+    apicontent.appendChild(borders_el);
+    apicontent.appendChild(population_el);
+    apicontent.appendChild(languages_el);
+    apicontent.appendChild(area_el);
+    apicontent.appendChild(region_el);
+    apicontent.appendChild(timezones_el);
+    apicontent.appendChild(map_el);
+  } 
+  catch (error) {
+    console.log(error);
+  }
+});
